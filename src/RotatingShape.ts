e2b6d5c860e2d8b1645d00ae3029e98af6101764
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2025-06-29 16:01:35 +0300

    Add toString

--- src/RotatingShape.ts
+++ src/RotatingShape.ts
@@ -8,8 +8,22 @@ export class RotatingShape {
         let resultRows: Array<Array<string>> = []
         for (const row of rows) {
             let trimmedRow = row.trim().split('')
-            resultRows = resultRows.concat(trimmedRow)
+            resultRows = resultRows.concat([trimmedRow])
         }
-        new RotatingShape(resultRows)
+        console.log(new RotatingShape(resultRows))
+        return new RotatingShape(resultRows)
+    }
+
+    toString = () => {
+        let shapeString = "";
+        for (const row of this.cells) {
+            let rowString = "";
+            for (const rowCell of row) {
+            rowString = rowString.concat(rowCell)
+            }
+            rowString = rowString.concat("\n");
+            shapeString = shapeString.concat(rowString);
+        }
+        return shapeString;
     }
 }

edacc82fe0151460c7d51012d2cd3681c094e6f7
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2025-06-29 15:41:01 +0300

    Add fromString

---
+++ src/RotatingShape.ts
@@ -0,0 +1,15 @@
+export class RotatingShape {
+    cells: Array<Array<string>>;
+    constructor(cells: Array<Array<string>>) {
+        this.cells = cells
+    }
+    static fromString = (str: string) => {
+        const rows = str.split('\n')
+        let resultRows: Array<Array<string>> = []
+        for (const row of rows) {
+            let trimmedRow = row.trim().split('')
+            resultRows = resultRows.concat(trimmedRow)
+        }
+        new RotatingShape(resultRows)
+    }
+}

9e8f2ebab67e494e69f5fc37852da68546e8ab7a
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2025-07-10 13:42:51 +0300

    Add OneByOneBlock

---
+++ src/OneByOneBlock.ts
@@ -0,0 +1,33 @@
+import Shape from "./Shape";
+
+type BlockShape = Array<Array<string>>
+type BlockOrientations = Array<BlockShape>
+
+const BLOCK_SHAPE = [
+    [['X']]
+]
+
+export class OneByOneBlock implements Shape {
+
+    BLOCK = new OneByOneBlock(BLOCK_SHAPE, 0)
+
+    blockOrientations: BlockOrientations
+    orientationNumber: number
+
+    constructor(block: BlockOrientations, orientation: number) {
+        this.blockOrientations = block
+        this.orientationNumber = orientation
+    }
+
+    toString = () => {
+        return this.blockOrientations[0][0][0]
+    }
+
+    rotateLeft = () => {
+        return new OneByOneBlock(this.blockOrientations, this.orientationNumber)
+    }
+
+    rotateRight = () => {
+        return new OneByOneBlock(this.blockOrientations, this.orientationNumber)
+    }
+}

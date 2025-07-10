6412ba8d63f859e5f52a5b2b0a5fceee981afca9
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2025-07-10 12:53:28 +0300

    Unskip O initial test

--- test/RotatingTetrominoes.test.ts
+++ test/RotatingTetrominoes.test.ts
@@ -86,7 +86,7 @@ describe("The I shape", () => {
   });
 });
 
-/*
+
 describe("The O shape", () => {
   const shape = Tetromino.O_SHAPE;
 
@@ -118,4 +118,3 @@ describe("The O shape", () => {
     expect(distinctOrientations(shape).size).to.equal(1);
   });
 });
-*/

d4c9cc49bbd213fe5c1dd7f5c586be91c8025d9e
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2025-07-10 11:41:24 +0300

    Add tetromino shapes

--- src/tetrominoShapes.ts
+++ src/tetrominoShapes.ts
@@ -3,3 +3,41 @@ export const TETROMINO_T = [
     ['T','T','T'],
     ['.','.','.'],
 ]
+
+export const TETROMINO_S = [
+    ['.','S','S'],
+    ['S','S','.'],
+    ['.','.','.'],
+]
+
+export const TETROMINO_Z = [
+    ['Z','Z','.'],
+    ['.','Z','Z'],
+    ['.','.','.'],
+]
+
+export const TETROMINO_L = [
+    ['.','.','L'],
+    ['L','L','L'],
+    ['.','.','.'],
+]
+
+export const TETROMINO_J = [
+    ['.','.','J'],
+    ['J','J','J'],
+    ['.','.','.'],
+]
+
+export const TETROMINO_I = [
+    ['.','.','.','.','.'],
+    ['.','.','.','.','.'],
+    ['I','I','I','I','.'],
+    ['.','.','.','.','.'],
+    ['.','.','.','.','.'],
+]
+
+export const TETROMINO_O = [
+    ['.','O','O'],
+    ['.','O','O'],
+    ['.','.','.'],
+]

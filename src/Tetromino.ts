8b200354f87dc7cd466510d01b72fb0c8ad9f8fa
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2025-07-10 11:44:51 +0300

    Add static shapes to Tetromino

--- src/Tetromino.ts
+++ src/Tetromino.ts
@@ -1,18 +1,22 @@
 import Shape, { reverseRows, transpose } from "./Shape";
-import { TETROMINO_T } from "./tetrominoShapes";
+import { TETROMINO_I, TETROMINO_J, TETROMINO_L, TETROMINO_O, TETROMINO_S, TETROMINO_T, TETROMINO_Z } from "./tetrominoShapes";
 
 type TetrominoShape = Array<Array<string>>
 
 export class Tetromino implements Shape {
-
+    static I_SHAPE = new Tetromino(TETROMINO_I)
+    static O_SHAPE = new Tetromino(TETROMINO_O)
     static T_SHAPE = new Tetromino(TETROMINO_T)
+    static J_SHAPE = new Tetromino(TETROMINO_J)
+    static L_SHAPE = new Tetromino(TETROMINO_L)
+    static S_SHAPE = new Tetromino(TETROMINO_S)
+    static Z_SHAPE = new Tetromino(TETROMINO_Z)
+
     cells: TetrominoShape;
 
     constructor(cells: TetrominoShape) {
         this.cells = cells
     }
-
-    
 
     toString = () => {
         let shapeString = "";

6fc56ee61bf6bbd779c6b1e457021f15128e72d6
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2025-07-11 11:21:52 +0300

    Add implement moveRight

--- src/Board.ts
+++ src/Board.ts
@@ -72,6 +72,10 @@ export class Board {
     moveLeft = () => {
         this.cells = moveAllFallingCellsLeft(this.cells)
     }
+
+    moveRight = () => {
+        this.cells = moveAllFallingCellsRight(this.cells)
+    }
 }
 
 const moveAllFallingCellsLeft = (cells: Cells) => {
@@ -82,6 +86,21 @@ const moveAllFallingCellsLeft = (cells: Cells) => {
         for (let col = 0; col < width; col++) {
             if (cells[row][col] === "f") {
                 cells[row][col - 1 ] = "f";
+                cells[row][col] = ".";
+            }
+        }
+    }
+    return cells
+}
+
+const moveAllFallingCellsRight = (cells: Cells) => {
+    const height = cells.length
+    const lastCol = cells[0].length - 1;
+
+    for (let row = 0; row < height; row++) {
+        for (let col = lastCol; col >= 0; col--) {
+            if (cells[row][col] === "f") {
+                cells[row][col + 1 ] = "f";
                 cells[row][col] = ".";
             }
         }

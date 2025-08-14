ce54928dbc84cd8d576bf68e482090d15536a297
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2025-08-14 14:17:23 +0300

    Add test for rotating left in empty space

--- test/RotatingFallingTetrominoes.test.ts
+++ test/RotatingFallingTetrominoes.test.ts
@@ -1,26 +1,44 @@
 import { beforeEach, describe, test } from "vitest";
 import { Board } from "../src/Board";
-import { Tetromino } from "../src/Tetromino";
 import { expect } from "chai";
+import { Tetromino } from "../src/Tetromino";
 
-describe("Rotating falling tetrominoes ", () => {
+describe("Rotating falling tetrominoes T can rotate ", () => {
     let board: Board;
 
     beforeEach(() => {
-        board = new Board(10, 6);
+        board = new Board(10, 6)
         board.drop(Tetromino.T_SHAPE)
+        board.moveDown()
     })
 
     test("tests have the correct setup", () => {
         expect(board.toString()).to.equalShape(
             `
+            ..........
             ....T.....
             ...TTT....
-            ..........
             ..........
             ..........
             ..........
             `
         );
     });
+
+    test("left in empty space", () => {
+        board.moveDown()
+
+        board.rotateLeft()
+
+        expect(board.toString()).to.equalShape(
+            `
+            ..........
+            ....T.....
+            ...TT.....
+            ....T.....
+            ..........
+            ..........
+            `
+        )
+    })
 })

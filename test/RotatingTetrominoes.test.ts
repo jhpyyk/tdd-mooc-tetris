63d724806c78fe1313960935b332a2ad5e2b286f
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2025-08-29 19:04:57 +0300

    Refactor test to use arika

--- test/RotatingTetrominoes.test.ts
+++ test/RotatingTetrominoes.test.ts
@@ -55,35 +55,38 @@ describe("The T shape", () => {
 });
 
 describe("The I shape", () => {
-    const shape = Tetromino.I_SHAPE;
+    const shape = Tetromino.ARIKA_I;
 
     test("initial orientation", () => {
         expect(shape.toString()).to.equalShape(
-            `.....
-       .....
-       IIII.
-       .....
-       .....`
+            `
+        ....
+        IIII
+        ....
+        ....
+        `
         );
     });
 
     test("can be rotated right/clockwise", () => {
         expect(shape.rotateRight().toString()).to.equalShape(
-            `..I..
-       ..I..
-       ..I..
-       ..I..
-       .....`
+            `
+        ..I.
+        ..I.
+        ..I.
+        ..I.
+        `
         );
     });
 
     test("can be rotated left/counter-clockwise", () => {
         expect(shape.rotateLeft().toString()).to.equalShape(
-            `..I..
-       ..I..
-       ..I..
-       ..I..
-       .....`
+            `
+        ..I.
+        ..I.
+        ..I.
+        ..I.
+        `
         );
     });
 

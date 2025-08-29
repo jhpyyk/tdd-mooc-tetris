edcc46cbe6401d0570f207ae9cb8dc833a80d551
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2025-08-29 10:36:58 +0300

    Add tests for right wall kick

--- test/SimpleWallKick.test.ts
+++ test/SimpleWallKick.test.ts
@@ -4,7 +4,7 @@ import { Tetromino } from "../src/Tetromino";
 import { setupFallingShape } from "./testUtils";
 import { expect } from "chai";
 
-describe("T can do a simple wall kick ", () => {
+describe("T can do a simple left wall kick ", () => {
     let board: Board;
 
     beforeEach(() => {
@@ -25,13 +25,80 @@ describe("T can do a simple wall kick ", () => {
         );
     });
 
-    test("rotating left when facing left wall", () => {
+    test("when rotating left", () => {
         board.rotateLeft();
 
         expect(board.toString()).to.equalShape(
             `
         .T........
         TTT.......
+        ..........
+        ..........
+        ..........
+        ..........
+        `
+        );
+    });
+
+    test("when rotating right", () => {
+        board.rotateRight();
+
+        expect(board.toString()).to.equalShape(
+            `
+        ..........
+        TTT.......
+        .T........
+        ..........
+        ..........
+        ..........
+        `
+        );
+    });
+});
+
+describe("T can do a simple right wall kick ", () => {
+    let board: Board;
+
+    beforeEach(() => {
+        board = new Board(10, 6);
+        setupFallingShape(board, Tetromino.T_SHAPE.rotateLeft(), 0, board.cells[0].length - 2);
+    });
+
+    test("tests have the correct setup", () => {
+        expect(board.toString()).to.equalShape(
+            `
+        .........T
+        ........TT
+        .........T
+        ..........
+        ..........
+        ..........
+        `
+        );
+    });
+
+    test("rotating left when facing right wall", () => {
+        board.rotateLeft();
+
+        expect(board.toString()).to.equalShape(
+            `
+        ..........
+        .......TTT
+        ........T.
+        ..........
+        ..........
+        ..........
+        `
+        );
+    });
+
+    test("rotating right when facing right wall", () => {
+        board.rotateRight();
+
+        expect(board.toString()).to.equalShape(
+            `
+        ........T.
+        .......TTT
         ..........
         ..........
         ..........

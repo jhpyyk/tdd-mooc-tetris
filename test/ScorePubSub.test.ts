cf96d9b303b3871b05685d4a16a656aa624ffb7f
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2025-09-05 14:42:10 +0300

    Add test for Score pub sub

---
+++ test/ScorePubSub.test.ts
@@ -0,0 +1,66 @@
+import * as chaiModule from "chai";
+import spies from "chai-spies";
+const chai = chaiModule.use(spies);
+import { beforeEach, describe, test } from "vitest";
+import { expect } from "chai";
+
+test("A Subscriber can be attached to a Publisher", () => {
+    const sub = new ScoreSubscriber("Score");
+    const pub = new ScorePublisher();
+
+    pub.attach(sub);
+
+    expect(pub.subscribers).to.include(sub);
+});
+
+// describe("A Publisher ", () => {
+//     let sub: MinimalSubscriber;
+//     let pub: MinimalPublisher;
+
+//     beforeEach(() => {
+//         sub = new MinimalSubscriber("Minimal Subscriber");
+//         pub = new MinimalPublisher();
+
+//         pub.attach(sub);
+//     });
+
+//     test("can list subscriber names", () => {
+//         expect(pub.list()).to.include("Minimal Subscriber");
+//     });
+
+//     test("can attach multiple subscribers", () => {
+//         const sub2 = new MinimalSubscriber("Minimal Subscriber 2");
+//         pub.attach(sub2);
+
+//         expect(pub.list()).to.include("Minimal Subscriber 2");
+//     });
+
+//     test("can only attach Subscribers with unique names", () => {
+//         const sub2 = new MinimalSubscriber("Minimal Subscriber");
+//         pub.attach(sub2);
+
+//         expect(pub.list().length).to.equal(1);
+//     });
+
+//     test("can detach a subscriber", () => {
+//         pub.detach(sub);
+
+//         expect(pub.list().length).to.equal(0);
+//     });
+
+//     test("can not detach a non-attached subscriber", () => {
+//         expect(pub.list().length).to.equal(1);
+//         const sub2 = new MinimalSubscriber("MS2");
+//         pub.detach(sub2);
+
+//         expect(pub.list().length).to.equal(1);
+//     });
+
+//     test("can publish a message", () => {
+//         const message = "message";
+//         const receiveSpy = chai.spy.on(sub, "receive");
+//         pub.publish(message);
+
+//         expect(receiveSpy).to.have.been.called.with(message);
+//     });
+// });

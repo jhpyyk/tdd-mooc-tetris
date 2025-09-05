b250944df883935b0737ce87fa42c53a14ee9c4d
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2025-09-05 15:12:41 +0300

    Add implementation for initial level

--- src/ScoringSystems/SimpleLineScoringSystem.ts
+++ src/ScoringSystems/SimpleLineScoringSystem.ts
@@ -1,7 +1,12 @@
 export class SimpleLineScoringSystem {
     private currentScore = 0;
+    private currentLevel = 1;
 
     getCurrentScore = () => {
         return this.currentScore;
+    };
+
+    getCurrentLevel = () => {
+        return this.currentLevel;
     };
 }

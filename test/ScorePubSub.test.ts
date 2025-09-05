a6a6beb541100825767070926256daa0caa666ba
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2025-09-05 14:52:15 +0300

    Add lineclear pub sub test

--- test/ScorePubSub.test.ts
+++ test/ScorePubSub.test.ts
@@ -37,12 +37,12 @@ describe("A Publisher ", () => {
         expect(pub.list()).to.include("LineClear2");
     });
 
-    //     test("can only attach Subscribers with unique names", () => {
-    //         const sub2 = new MinimalSubscriber("Minimal Subscriber");
-    //         pub.attach(sub2);
+    test("can only attach Subscribers with unique names", () => {
+        const sub2 = new LineClearSubscriber("LineClear");
+        pub.attach(sub2);
 
-    //         expect(pub.list().length).to.equal(1);
-    //     });
+        expect(pub.list().length).to.equal(1);
+    });
 
     //     test("can detach a subscriber", () => {
     //         pub.detach(sub);

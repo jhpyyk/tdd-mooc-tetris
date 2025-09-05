e84a23ddf88d92a25f4e0ef178722fe2888fbe26
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2025-09-05 15:09:48 +0300

    Add implementation for initial score

---
+++ src/ScoringSystems/SimpleLineScoringSystem.ts
@@ -0,0 +1,7 @@
+export class SimpleLineScoringSystem {
+    private currentScore = 0;
+
+    getCurrentScore = () => {
+        return this.currentScore;
+    };
+}
--- test/Scoring.test.ts
+++ test/Scoring.test.ts
@@ -1,5 +1,6 @@
 import { expect } from "chai";
 import { describe, test } from "vitest";
+import { SimpleLineScoringSystem } from "../src/ScoringSystems/SimpleLineScoringSystem";
 
 describe("The scoring system ", () => {
     test("has an initial score of 0", () => {

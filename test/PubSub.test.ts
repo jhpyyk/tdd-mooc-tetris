1723c8337ac6e55f166a9c13486dde527e7a45c2
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2025-09-05 12:18:28 +0300

    Refactor pub sub attach test

--- test/PubSub.test.ts
+++ test/PubSub.test.ts
@@ -1,13 +1,13 @@
 import { expect } from "chai";
-import { describe } from "vitest";
+import { describe, test } from "vitest";
 import { MinimalSubscriber } from "../src/Subscribers/MinimalSubscriber";
 import { MinimalPublisher } from "../src/Publishers/MinimalPublisher";
 
-describe("A Subscriber can be attached to a Publisher", () => {
+test("A Subscriber can be attached to a Publisher", () => {
     const sub = new MinimalSubscriber();
     const pub = new MinimalPublisher();
 
     pub.attach(sub);
 
-    expect(pub.subscribers.length).to.equal(1);
+    expect(pub.subscribers.length).to.include(sub);
 });

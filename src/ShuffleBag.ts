d399b544790b414b4b20bd79f02910eeeef8027b
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2025-09-07 16:16:08 +0300

    Add a hardcoded pull

--- src/ShuffleBag.ts
+++ src/ShuffleBag.ts
@@ -3,8 +3,14 @@ import { ShapeChar } from "./Shape";
 export class ShuffleBag {
     chars: ShapeChar[];
     length: number;
+    contents: ShapeChar[];
     constructor() {
         this.chars = ["I", "J", "L", "O", "S", "T", "Z"];
         this.length = this.chars.length;
+        this.contents = this.chars;
     }
+
+    pull = () => {
+        return this.contents[0];
+    };
 }

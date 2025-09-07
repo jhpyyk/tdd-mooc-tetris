069e4e5d8efe5c00febcbb09be95dc487a898c30
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2025-09-07 16:42:42 +0300

    Add bag shuffling

--- src/ShuffleBag.ts
+++ src/ShuffleBag.ts
@@ -4,10 +4,12 @@ export class ShuffleBag {
     chars: ShapeChar[];
     length: number;
     contents: ShapeChar[];
-    constructor() {
+    rngSeed: number;
+    constructor(seed: number) {
         this.chars = ["I", "J", "L", "O", "S", "T", "Z"];
         this.length = this.chars.length;
-        this.contents = this.chars;
+        this.rngSeed = seed;
+        this.contents = shuffle(this.chars, seed);
     }
 
     pull = () => {
@@ -16,3 +18,28 @@ export class ShuffleBag {
         return toBePulled;
     };
 }
+
+// modified from
+// https://stackoverflow.com/questions/16801687/javascript-random-ordering-with-seed
+const shuffle = (array: Array<ShapeChar>, seed: number) => {
+    let m = array.length;
+
+    // While there remain elements to shuffle…
+    while (m) {
+        // Pick a remaining element…
+        let i = Math.floor(random(seed) * m--);
+
+        // And swap it with the current element.
+        let t = array[m];
+        array[m] = array[i];
+        array[i] = t;
+        ++seed;
+    }
+
+    return array;
+};
+
+const random = (seed: number) => {
+    let x = Math.sin(seed++) * 10000;
+    return x - Math.floor(x);
+};
--- test/ShuffleBag.test.ts
+++ test/ShuffleBag.test.ts
@@ -1,9 +1,14 @@
 import { expect } from "chai";
-import { describe, test } from "vitest";
+import { beforeEach, describe, test } from "vitest";
 import { ShuffleBag } from "../src/ShuffleBag";
+import { ShapeChar } from "../src/Shape";
 
 describe("Shuffle bag ", () => {
-    const bag = new ShuffleBag();
+    let bag: ShuffleBag;
+
+    beforeEach(() => {
+        bag = new ShuffleBag(123456);
+    });
     test("length is 7", () => {
         expect(bag.length).to.equal(7);
     });
@@ -19,5 +24,19 @@ describe("Shuffle bag ", () => {
         const char2 = bag.pull();
 
         expect(char1).not.to.equal(char2);
+    });
+
+    test("pulled characters are the same for the same seed", () => {
+        const bag2 = new ShuffleBag(123456);
+
+        let charsFromBag1: ShapeChar[] = [];
+        let charsFromBag2: ShapeChar[] = [];
+
+        for (let i = 0; i < bag.length; i++) {
+            charsFromBag1 = charsFromBag1.concat(bag.pull());
+            charsFromBag2 = charsFromBag2.concat(bag2.pull());
+        }
+
+        expect(charsFromBag1).to.be.deep.equal(charsFromBag2);
     });
 });
